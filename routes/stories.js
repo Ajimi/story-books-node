4de1815d1c575cb016a243a1751baf9a63043993
Selim <sloumma.21@gmail.com>
2018-10-15 09:11:13 +0100

    Story Model  and Create functionality

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
 const router = express.Router();
-const {ensureAuthenticated} = require('../helpers/auth');
+const {
+    ensureAuthenticated
+} = require('../helpers/auth');
+
 
 
 // Stories Index
@@ -11,6 +17,31 @@ router.get('/', (req, res) => {
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
+});
+
+
+// Process Add Story
+router.post('/', (req, res) => {
+    let allowComment;
+
+    if (req.body.allowComments) {
+        allowComment = true;
+    } else {
+        allowComment = false;
+    }
+
+    const newStory = {
+        title: req.body.title,
+        status: req.body.status,
+        allowComments: allowComment,
+        body: req.body.body,
+        user: req.user.id
+    }
+    new Story(newStory)
+        .save()
+        .then(story => {
+            res.redirect(`/stories/show/${story._id}`);
+        })
 });
 
 module.exports = router;
