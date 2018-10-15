9ce4f04614f989c40512e2ae50991b8840267688
Selim <sloumma.21@gmail.com>
2018-10-15 12:26:06 +0100

    Add test

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    mongoURI: 'mongodb://ajimi:ajimi123@ds131903.mlab.com:31903/story-books-dev',
+    googleClientID:'685529647047-jdppifkses1rt4arn5n9m8frpfnm1bh6.apps.googleusercontent.com',
+    googleClientSecret:'6utnYNqzNdBkGmB1Bhfdm9ub'
 };
