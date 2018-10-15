b2a92c2ce631ddc7c4c185e842e00c4e171bd06f
Selim <sloumma.21@gmail.com>
2018-10-15 12:30:45 +0100

    Update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports == require('./keys_prod');
-} else {
-    module.exports = require('./keys_dev');
-}
+module.exports = {
+    mongoURI: 'mongodb://ajimi:ajimi123@ds131903.mlab.com:31903/story-books-dev',
+    googleClientID:'685529647047-jdppifkses1rt4arn5n9m8frpfnm1bh6.apps.googleusercontent.com',
+    googleClientSecret:'6utnYNqzNdBkGmB1Bhfdm9ub'
+};
